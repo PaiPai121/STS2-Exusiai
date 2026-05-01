f5e22d6b296e2edc0e32cbdf8799ca5831ef916a
paipai121 <kunmeigo@gmail.com>
2026-05-01 15:29:07 +0800

    Refactor AngelsBlessing into proper Power system

--- Code/Cards/AngelsBlessing.cs
+++ Code/Cards/AngelsBlessing.cs
@@ -5,21 +5,18 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MyFirstMod.Code.CardPools;
+using MyFirstMod.Code.Powers;
 
 namespace MyFirstMod.Code.Cards;
 
 [Pool(typeof(ExusiaiCardPool))]
 public class AngelsBlessing : MyFirstModCardModel
 {
-    private int _cardsPlayedThisTurn;
-    private bool _blessingActive;
-
     private const int energyCost = 1;
     private const CardType type = CardType.Power;
     private const CardRarity rarity = CardRarity.Uncommon;
     private const TargetType targetType = TargetType.Self;
     private const bool shouldShowInCardLibrary = true;
-
 
     public override IEnumerable<DynamicVar> CanonicalVars => [
         new CardsVar(1)
@@ -33,41 +30,12 @@ public class AngelsBlessing : MyFirstModCardModel
 
     public override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        _cardsPlayedThisTurn = 0;
-        _blessingActive = true;
         await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.IntValue, Owner);
-    }
-
-    public override Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, MegaCrit.Sts2.Core.Entities.Players.Player player)
-    {
-        if (Owner == player)
-        {
-            _cardsPlayedThisTurn = 0;
-            _blessingActive = false;
-        }
-
-        return Task.CompletedTask;
-    }
-
-    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
-    {
-        if (!_blessingActive)
-            return;
-
-        if (cardPlay.Card == null)
-            return;
-
-        if (Owner == null || cardPlay.Card.Owner != Owner)
-            return;
-
-        if (cardPlay.Card == this)
-            return;
-
-        _cardsPlayedThisTurn++;
-        if (_cardsPlayedThisTurn % 5 != 0)
-            return;
-
-        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.IntValue, Owner);
+        await PowerCmd.Apply<AngelsBlessingPower>(
+            Owner.Creature,
+            DynamicVars.Cards.IntValue,
+            Owner.Creature,
+            this);
     }
 
     public override void OnUpgrade()
---
+++ Code/Powers/AngelsBlessingPower.cs
@@ -0,0 +1,35 @@
+using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace MyFirstMod.Code.Powers;
+
+public class AngelsBlessingPower : CustomPowerModel
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    public override List<(string, string)> Localization => [("title", "天使祝福"), ("description", "每打出5张牌，抽[blue]{Cards}[/blue]张牌。"), ("smartDescription", "每打出5张牌，抽[blue]{Cards}[/blue]张牌。")];
+
+    private int _cardsPlayedThisTurn;
+
+    public override Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        _cardsPlayedThisTurn = 0;
+        return Task.CompletedTask;
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Card == null) return;
+
+        _cardsPlayedThisTurn++;
+        if (_cardsPlayedThisTurn % 5 != 0) return;
+
+        var drawAmount = DynamicVars[nameof(AngelsBlessingPower)].BaseValue;
+        await CardPileCmd.Draw(choiceContext, drawAmount, Owner.Player);
+    }
+}
