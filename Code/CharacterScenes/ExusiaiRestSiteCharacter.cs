952811d08f96a4434e50933105f7796ba249c46d
paipai121 <kunmeigo@gmail.com>
2026-05-30 15:58:44 +0800

    Use native rest site scene for Exusiai

--- Code/CharacterScenes/ExusiaiRestSiteCharacter.cs
+++
@@ -1,7 +0,0 @@
-using MegaCrit.Sts2.Core.Nodes.RestSite;
-
-namespace MyFirstMod.Code.CharacterScenes;
-
-public class ExusiaiRestSiteCharacter : NRestSiteCharacter
-{
-}
--- Code/CharacterScenes/ExusiaiSelectionReticle.cs
+++
@@ -1,7 +0,0 @@
-using MegaCrit.Sts2.Core.Nodes.Combat;
-
-namespace MyFirstMod.Code.CharacterScenes;
-
-public class ExusiaiSelectionReticle : NSelectionReticle
-{
-}
